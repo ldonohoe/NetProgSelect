a85e84fc958939e581969f07bb584717e72988df
Andrew <adleaf1@gmail.com>
2018-10-05 00:02:25 -0400

    Changing server port works

--- lab3_client.c
+++ lab3_client.c
@@ -6,13 +6,15 @@ main(int argc, char **argv)
 	int					sockfd;
 	struct sockaddr_in	servaddr;
 
-	if (argc != 2)
+	/*if (argc != 2)
 		err_quit("usage: udpcli <IPaddress>");
+	*/
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	//Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	Inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
 
 	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
--- lab3_server.c
+++ lab3_server.c
@@ -6,12 +6,16 @@ main(int argc, char **argv)
 	int					sockfd;
 	struct sockaddr_in	servaddr, cliaddr;
 
+	int addToPort;
+	sscanf(argv[1], "%d", &addToPort);
+	printf("The number to be added is %d\n", addToPort);
+
 	sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(SERV_PORT);
+	servaddr.sin_port        = htons(SERV_PORT + addToPort);
 
 	Bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
