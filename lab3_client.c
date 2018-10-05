765c39ffb901dced01ea0c4c3aeafdd0df601792
Andrew <adleaf1@gmail.com>
2018-10-05 12:20:42 -0400

    Server complete, begun client

--- lab3_client.c
+++ lab3_client.c
@@ -3,16 +3,17 @@
 int
 main(int argc, char **argv)
 {
+	/*
 	int					sockfd;
 	struct sockaddr_in	servaddr;
 
-	/*if (argc != 2)
-		err_quit("usage: udpcli <IPaddress>");
-	*/
+	//if (argc != 2)
+	//	err_quit("usage: udpcli <IPaddress>");
+	
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_port = htons(9881);//SERV_PORT);
 	//Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 	Inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
 
@@ -21,4 +22,118 @@ main(int argc, char **argv)
 	dg_cli(stdin, sockfd, (SA *) &servaddr, sizeof(servaddr));
 
 	exit(0);
+	*/
+
+	/*
+	const char* server_name = "127.0.0.1";
+	const int server_port = argv[1];
+
+	struct sockaddr_in	servaddr;
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+
+	// creates binary representation of server name
+	// and stores it as sin_addr
+	Inet_pton(AF_INET, server_name, &servaddr.sin_addr);
+
+	// htons: port in network order format
+	server_address.sin_port = htons(server_port);
+
+	// open a stream socket
+	int sock;
+	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("could not create socket\n");
+		return 1;
+	}
+
+	// TCP is connection oriented, a reliable connection
+	// **must** be established before any data is exchanged
+	if (connect(sock, (struct sockaddr*)&server_address,
+	            sizeof(server_address)) < 0) {
+		printf("could not connect to server\n");
+		return 1;
+	}
+
+	// send
+
+	// data that will be sent to the server
+	const char* data_to_send = "connect";
+	send(sock, data_to_send, strlen(data_to_send), 0);
+
+	// receive
+
+	int n = 0;
+	int len = 0, maxlen = 100;
+	char buffer[maxlen];
+	char* pbuffer = buffer;
+
+	// will remain open until the server terminates the connection
+	while ((n = recv(sock, pbuffer, maxlen, 0)) > 0) {
+		pbuffer += n;
+		maxlen -= n;
+		len += n;
+
+		buffer[len] = '\0';
+		printf("received: '%s'\n", buffer);
+	}
+
+	// close the socket
+	close(sock);
+	return 0;
+	*/
+	/*
+	int					sockfd;
+	struct sockaddr_in	servaddr;
+	char* localhost = "127.0.0.1";
+	int port = 9881;
+
+	//if (argc != 2)
+	//	err_quit("usage: tcpcli <IPaddress>");
+
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	Inet_pton(AF_INET, localhost, &servaddr.sin_addr);
+
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	//str_cli(stdin, sockfd);		
+	char	sendline[MAXLINE], recvline[MAXLINE];
+
+	while (1) {
+
+		//Writen(sockfd, sendline, strlen(sendline));
+
+		if (Readline(sockfd, recvline, MAXLINE) == 0)
+			err_quit("str_cli: server terminated prematurely");
+
+		Fputs(recvline, stdout);
+	}
+
+	exit(0);
+	*/
+	int					sockfd;
+	struct sockaddr_in	servaddr;
+	char* localhost = "127.0.0.1";
+
+	//if (argc != 2)
+	//	err_quit("usage: tcpcli <IPaddress>");
+
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	Inet_pton(AF_INET, localhost, &servaddr.sin_addr);
+
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	str_cli(stdin, sockfd);		/* do it all */
+
+	exit(0);
+
+
+
 }
--- lab3_server.c
+++ lab3_server.c
@@ -3,6 +3,7 @@
 int
 main(int argc, char **argv)
 {
+	/*
 	int					sockfd;
 	struct sockaddr_in	servaddr, cliaddr;
 
@@ -19,5 +20,71 @@ main(int argc, char **argv)
 
 	Bind(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-	dg_echo(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
+	//dg_echo(sockfd, (SA *) &cliaddr, sizeof(cliaddr));
+
+
+
+	//int			n;
+	//socklen_t	len;
+	//char		mesg[MAXLINE];
+
+	char sendString[100];
+
+
+
+	for ( ; ; ) {
+		//len = clilen;
+		//n = Recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+		scanf("%s", sendString);
+		//fgets(sendString, 100, stdin);
+
+		printf("%s\n", sendString);
+
+		//Sendto(sockfd, mesg, n, 0, pcliaddr, clilen);
+	}
+	*/
+	int					listenfd, connfd;
+	pid_t				childpid;
+	socklen_t			clilen;
+	struct sockaddr_in	cliaddr, servaddr;
+
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(SERV_PORT);
+
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+	Listen(listenfd, LISTENQ);
+
+	clilen = sizeof(cliaddr);
+	connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+
+	char sendString[100];
+
+	for ( ; ; ) {
+		
+		scanf("%s", sendString);
+		//printf("%s\n", sendString);
+		//write(connfd, sendString, sizeof(sendString));
+		//sendto(connfd, sendString, sizeof(sendString));
+		int i = 0;
+		printf("The length is %lu\n", strlen(sendString));
+		for (i = 0; i < strlen(sendString); i++) {
+			printf("%c\n", sendString[i]);
+		}
+
+		//printf("test\n");
+		//if ( (childpid = Fork()) == 0) {	/* child process */
+		//Close(listenfd);	/* close listening socket */
+		//str_echo(connfd);	/* process the request */
+		//Writen(connfd, "This is a test");
+		//exit(0);
+		//}
+		//Close(connfd);			/* parent closes connected socket */
+	}
+
+
 }
