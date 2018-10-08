86acaf337fcec56efdd6bdaafd5ac46b29c818d4
Andrew <adleaf1@gmail.com>
2018-10-08 01:30:24 -0400

    Client will now print when the server closes

--- lab3_client3.c
+++ lab3_client3.c
@@ -9,7 +9,51 @@
 
 #include "lib/unpv13e/lib/unp.h"
 
+int
+main(int argc, char **argv)
+{
+	int					sockfd;
+	struct sockaddr_in	servaddr;
+	char* address = "127.0.0.1";
+	int port = 9881;
+
+	//if (argc != 2)
+	//	err_quit("usage: tcpcli <IPaddress>");
+
+	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);//SERV_PORT);
+	Inet_pton(AF_INET, address, &servaddr.sin_addr);
+
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+	char sendline[MAXLINE], recvline[MAXLINE];
+
+	while (1) {
+
+
+		if (Readline(sockfd, recvline, MAXLINE) == 0) {
+			err_quit("str_cli: server terminated prematurely");
+		}
+
+		//Fputs(recvline, stdout);
+		printf("%s\n", recvline);
+		fflush(stdout);
+		
+		
+	}
+
+	exit(0);
+}
+
+
+
+
 //int clientSelect(char **ipAddr ,int port , int count)
+
+/*
 int main(int argc, char *argv[])
 {   
 
@@ -82,3 +126,4 @@ int main(int argc, char *argv[])
     return ret ;
 
 }
+*/
